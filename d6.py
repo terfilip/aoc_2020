8157f2cbcb5d34d60aa0b2ad59c139687e9adde2
Filip Ter <filip.ter@gmail.com>
2020-12-06 04:56:32 -0800

    cleaning up whitespace

--- d6.py
+++ d6.py
@@ -1,8 +1,6 @@
 answers = []
 
-
 with open('6.txt', 'r') as f:
-
     chunk = []
     
     while line := f.readline():
@@ -13,16 +11,12 @@ with open('6.txt', 'r') as f:
         else:
             answers.append(chunk)
             chunk = []
-
     answers.append(chunk)
 
-
 def n_all_ans(grp):
-    
     ltop = {}
 
     for person, qans in enumerate(grp):
-        
         for ltr in qans:
             try:
                 ltop[ltr] = ltop[ltr].union({person})
@@ -31,8 +25,6 @@ def n_all_ans(grp):
 
     all_ans = {k: v for k, v in ltop.items() if v == set(range(len(grp)))}
     return len(all_ans)
-
-
 
 print('P1: ', sum((len(set(''.join(grp_ans))) for grp_ans in answers)))
 print('P2: ', sum((n_all_ans(grp_ans) for grp_ans in answers)))
