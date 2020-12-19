431dd1877eaee64f73c47214d39db35496b065ce
Filip Ter <filip.ter@gmail.com>
2020-12-19 22:08:08 +0000

    19.2

--- d19.py
+++ d19.py
@@ -1,4 +1,5 @@
 import itertools
+import re
 from copy import deepcopy
 
 def read_in(fname):
@@ -38,75 +39,23 @@ s1rules, s1seqs = read_in("19sample1.txt")
 s2rules, s2seqs = read_in("19sample2.txt")
 
 
-def combine2_vals(la,lb):
-
-    if la == str and lb == str:
-        return la + lb
-
-    if lb == str:
-        lb = [lb]
-
-    if la == str:
-        la = [la]
-
-    l = itertools.product(la, lb)
-    return [''.join(i) for i in l]
-
-
-def combine_vals(*vals):
-
-    if all(type(x) == str for x in vals):
-        return ''.join(vals)
-
-    vls = [[x] if type(x) == str else x for x in vals]
-    try:
-        return [''.join(x) for x in itertools.product(*vls)]
-    except TypeError:
-        print(list(itertools.product(*vls)))
-        print(vls)
-        print(vals)
-        raise
-
-
 def find_rules_seq(inrules, rule, rec_lim, target_seqs):
     call_ctr = {8: 0, 11: 0}
-    inrules = deepcopy(inrules)
     memo = {}
-    #inrules[8] = ([42], [42, ([42], [42])])bbbbb
-    #inrules[11] = ([42, 31], [42, ([42, 31], [42, 31]), 31])
-    #init_loop_rule = {8: ([42], [42]),
-    #                  11: ([42, 31], [42, 31])}
-    MAX_WRITES = 4
 
     def _find_rules_seq(rule):
         ruleval = inrules[rule]
-        #print('Rule: ', rule)
-
-        if rule in memo.keys():
-            return memo[rule]
 
         if rule in [8, 11] and call_ctr[rule] < rec_lim:
-            #print(call_ctr)
             call_ctr[rule] += 1
         elif rule in [8,11] and call_ctr[rule] >= rec_lim:
             if rule == 8:
-                print('rule 8 limit')
-                inrules[8] = 42
-                ans = memo[42]
-                memo[8] = ans
-                #ans = [s2 for s2 in ans if any(s2 in target_seq for target_seq in target_seqs)]
-                #print(ans)
-                return ans
+                return memo[42]
             elif rule == 11:
-                print('rule 11 limit')
-                inrules[11] = [42, 31]
-                found = [memo[42], memo[31]]
-                ans = combine_vals(*found)
-                memo[11] = ans
-                #ans = [s2 for s2 in ans if any(s2 in target_seq for target_seq in target_seqs)]
-                #print(ans)
-                #print('herrow')
-                return ans
+                base_case = "".join([memo[42], memo[31]])
+                return base_case
+        elif rule in memo.keys():
+            return memo[rule]
 
         if type(ruleval) == str:
             ans = ruleval.strip()
@@ -117,7 +66,7 @@ def find_rules_seq(inrules, rule, rec_lim, target_seqs):
 
             if len(ruleval[0]) > 1:
                 l2 = _find_rules_seq(ruleval[0][1])
-                l = combine_vals(l1, l2)
+                l = "".join([l1,l2])
             else:
                 l = l1
             r1 = _find_rules_seq(ruleval[1][0])
@@ -125,45 +74,28 @@ def find_rules_seq(inrules, rule, rec_lim, target_seqs):
             if len(ruleval[1]) > 1:
                 r2 = _find_rules_seq(ruleval[1][1])
 
-                if len(ruleval[1]) > 2: # Part 2 rule 11 only
+                if len(ruleval[1]) > 2:
                     r3 = _find_rules_seq(ruleval[1][2])
-                    r = combine_vals(r1, r2, r3)
+                    r = "".join([r1,r2,r3])
                 else:
-                    r = combine_vals(r1, r2)
+                    r = "".join([r1,r2])
             else:
                 r = r1
-
-            if type(l) == str:
-                l = [l]
-
-            if type(r) == str:
-                r = [r]
-
-            ans = l + r
+            ans = f"({l}|{r})"
         elif type(ruleval) == list:
             found = [_find_rules_seq(x) for x in ruleval]
-            ans = combine_vals(*found)
+            ans = "".join(found)
 
-        # If the generated strings are not substrings of any of the target sequences,
-        # then the strings they produce will not be any of the target sequences so they can be discarded
-        ans = [s2 for s2 in ans if any(s2 in target_seq for target_seq in target_seqs)]
         memo[rule] = ans
         return ans
 
-    return _find_rules_seq(rule)
+    ptn = _find_rules_seq(rule)
+    rgx = re.compile(ptn)
+    cnt = len([s for s in target_seqs if rgx.fullmatch(s)])
+
+    return cnt, rgx
 
 
-def get_ans(rules, seqs, reclim):
-    ans = find_rules_seq(rules, 0, reclim, seqs)
-    valid_seqs = [s for s in seqs if s in ans]
-    return len(valid_seqs), valid_seqs
-
-if False:
-    def p1():
-        allowed_seqs1 = find_rules_seq(rules, 0)
-        return len([seq for seq in seqs if seq in allowed_seqs1])
-
-    print('P1: ', p1())
-
-
+print('P1: ', find_rules_seq(rules, 0, 1, seqs)[0])
+print('P2: ', find_rules_seq(rulesv2, 0, 10, seqsv2)[0])
 
